40d9ab1949d1f5786d200755a13fe24971f54b4d
Wreck-X <hemanthkrishnakumar@gmail.com>
2023-10-10 08:16:43 +0530

    styling and placeholder for img

--- script.js
+++ script.js
@@ -18,8 +18,8 @@ app.get('/', (req, res) => {
       const filePath = path.join(directoryPath, file);
       try {
         const fileContent = fs.readFileSync(filePath, 'utf8');
-        return `${fileContent}`;
-      } catch (err) {
+        return `${fileContent}</li>`;
+      } catch (err) { pvjj
         console.error(`Error reading file ${file}:`, err);
         return `<li>${file} (Error reading file)</li>`;
       }
@@ -29,6 +29,45 @@ app.get('/', (req, res) => {
       <html>
       <head>
         <title>File List</title>
+        <style>
+            body{
+                margin: 0;
+                padding: 0;
+                background-color: rgb(15,9,19);
+                display: flex;
+                justify-content: center;
+            }
+            .container{
+                margin: 10px;
+                display: flex;
+                width: 50rem;
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+                background-color:rgba(19, 27, 54, 0.3);
+                padding: 20px;
+                border-radius: 20px;
+            }
+
+            .rounded{
+                border-radius: 100%;
+                height: 100px;
+                width: 100px;
+                background-color: white;
+            }
+            .card {        
+                align-items: center;
+                padding: 10px;
+                gap: 24px;
+                display: flex;
+                color: white;
+                font-size: 24px;
+                border-bottom: 2px solid rgb(19, 27, 54);
+            }
+            .card:last-child{
+                border-bottom: 0px solid white;
+            }
+        </style>
       </head>
       <body>
         <div class="container">
