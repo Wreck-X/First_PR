87968aae5a796fade968911af1de3489a465690e
govindkrishna03 <govindkrishna28@gmail.com>
2023-10-10 18:18:54 +0530

    added readme file

--- script.js
+++ script.js
@@ -1,54 +1,63 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 3000;
-const directoryPath = './your-cards/';
-app.get('/', (req, res) => {
-
+const directoryPath = "./your-cards/";
+app.get("/", (req, res) => {
   fs.readdir(directoryPath, (err, files) => {
     if (err) {
-      console.error('Error reading directory:', err);
-      return res.status(500).send('Internal Server Error');
+      console.error("Error reading directory:", err);
+      return res.status(500).send("Internal Server Error");
     }
 
-
-    const fileList = files.map((file) => {
-      const filePath = path.join(directoryPath, file);
-      try {
-        const fileContent = fs.readFileSync(filePath, 'utf8');
-        return `${fileContent}</li>`;
-      } catch (err) { pvjj
-        console.error(`Error reading file ${file}:`, err);
-        return `<li>${file} (Error reading file)</li>`;
-      }
-    }).join('');
+    const fileList = files
+      .map((file) => {
+        const filePath = path.join(directoryPath, file);
+        try {
+          const fileContent = fs.readFileSync(filePath, "utf8");
+          return `${fileContent}</li>`;
+        } catch (err) {
+          pvjj;
+          console.error(`Error reading file ${file}:`, err);
+          return `<li>${file} (Error reading file)</li>`;
+        }
+      })
+      .join("");
     const htmlResponse = `
       <!DOCTYPE html>
       <html>
       <head>
         <title>File List</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@300;400;600&display=swap" rel="stylesheet">
         <style>
-            body{
-                margin: 0;
-                padding: 0;
-                background-color: rgb(15,9,19);
-                display: flex;
-                justify-content: center;
-            }
-            .container{
-                margin: 10px;
-                display: flex;
-                width: 50rem;
-                flex-direction: column;
-                justify-content: center;
-                align-items: center;
-                background-color:rgba(19, 27, 54, 0.3);
-                padding: 20px;
-                border-radius: 20px;
-            }
+        body {
+          margin: 0;
+          padding: 0;
+          background-color: #0f0913;
+          display: flex;
+          justify-content: center;
+          font-family: 'JetBrains Mono', monospace;
+      }
+      
 
+    
+    .container {
+        margin: 30px; 
+        display: flex;
+        width: 50rem;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        background-color: rgba(19, 27, 54, 0.3);
+        padding: 30px;
+        border-radius: 20px;
+        border: 2px solid white;
+    }
+    
             .rounded{
                 border-radius: 100%;
                 height: 100px;
@@ -73,6 +82,7 @@ app.get('/', (req, res) => {
         <div class="container">
           ${fileList}
         </div>
+        
       </body>
       </html>
     `;
@@ -84,4 +94,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
